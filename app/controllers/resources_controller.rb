d6e30e8dc97b22580ff17d2fe28adf7a051f001d
Franco Sellitto <franco.sellitto@zhdk.ch>
2011-09-06 15:01:48 +0200

    using cancan load_resource

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -2,7 +2,6 @@
 class ResourcesController < ApplicationController
   #tmp# respond_to :html, :js
   
-  #mongo#
   load_resource :class => "Media::Resource"
   #load_and_authorize_resource :class => "Media::Resource"
 
@@ -12,12 +11,6 @@ class ResourcesController < ApplicationController
     resources = resources.where(:_id.in => current_user.favorite_resource_ids) if request.fullpath =~ /favorites/
     resources = resources.csearch(params[:query]) unless params[:query].blank?
 
-    #tmp#
-    #resources = Media::Resource.search("zhdk").accessible_by_subject(user)
-    #resources = Media::Resource.accessible_by(current_ability).csearch("zhdk")
-    #resources.limit(2).offset(3) #resources.count => total_entries #resources.size => total in page
-
-    #@json = resources.as_json(:except => :_keywords)
     @json = { :pagination => { :current_page => resources.current_page,
                                :per_page => resources.size,
                                :total_entries => resources.total_count,
@@ -34,9 +27,6 @@ class ResourcesController < ApplicationController
   end
 
   def show
-    #@resource = Media::Resource.find(params[:id])
-    #@resource = Media::Resource.accessible_by(current_ability).find(params[:id])
-    #@resource = current_user.media_entries.find(params[:id])
     authorize! :read, @resource => Media::Resource
 
     if @resource.is_a? Media::Set
@@ -56,7 +46,6 @@ class ResourcesController < ApplicationController
   end
 
   def edit
-    #@resource = Media::Resource.find(params[:id])
     authorize! :update, @resource => Media::Resource
 
     respond_to do |format|
