1157824fe458841f5ad1b393317d1b53a5cd7c01
Franco Sellitto <franco.sellitto@zhdk.ch>
2011-09-29 13:55:49 +0200

    bigger font on qtip

--- app/helpers/media/sets_helper.rb
+++ app/helpers/media/sets_helper.rb
@@ -51,6 +51,9 @@ module Media
               haml_tag :a, thumb_for(media_set, :small_125), :href => resource_path(media_set), :class => thumb_class
             end
           end
+          haml_tag :style do
+            haml_concat ".ui-tooltip { font-size: 1.1em; line-height: normal; }"
+          end
           script = javascript_tag do
             begin
             <<-HERECODE
