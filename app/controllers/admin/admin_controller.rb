9679307f02729192c08354bead4aef25ef3bb11a
Franco Sellitto <franco.sellitto@zhdk.ch>
2011-10-31 13:49:37 +0100

    removing hardcoded administrators

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -12,15 +12,8 @@ class Admin::AdminController < ApplicationController
     # OPTIMIZE
     required_group = Group.find_or_create_by(:name => "Admin")
     unless current_user.groups.is_member?(required_group)
-      # 10262 => Ramon Cahenzli
-      # 10301 => Susanne Schumacher
-      # 159123 => Franco Sellitto
-      if [10262, 10301, 159123].include?(current_user.id)
-        required_group.users << current_user
-      else
-        flash[:error] = "The function you wish to use is only available to admin users"
-        redirect_to root_path
-      end
+      flash[:error] = "The function you wish to use is only available to admin users"
+      redirect_to root_path
     end
   end
   
