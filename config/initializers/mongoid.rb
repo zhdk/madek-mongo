118e6664ab980cdde999f04412f7bf2feba4d0a9
Franco Sellitto <franco.sellitto@zhdk.ch>
2011-11-02 00:43:13 +0100

    custom parameterize_keys (mongoid settings)

---
+++ config/initializers/mongoid.rb
@@ -0,0 +1,12 @@
+# From: http://blog.noizeramp.com/2010/12/21/friendly-composite-keys-in-mongoid/
+# Instead of the standard composition, convert everything
+# non-alpha and non-digit to dash and squeeze
+class String
+  def identify
+    if Mongoid.parameterize_keys
+      gsub(/[^a-z0-9]+/, ' ').strip.gsub(' ', '-')
+    else
+      self
+    end
+  end
+end
